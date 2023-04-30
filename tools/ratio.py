9bf23ee9fa91b44d60fa90e280c4a1144837c623
LDD <2435206827@qq.com>
2023-04-30 13:47:13 +0800

    feat: 完成ratio.py数据清洗工具构建

--- tools/ratio.py
+++ tools/ratio.py
@@ -5,59 +5,61 @@ from PIL import Image
 from alive_progress import alive_bar
 from colorama import Fore
 import torchvision.transforms as transforms
-import numpy as np
-import torch.nn.functional as F
 
-class SquarePad:
-    def __call__(self, image):
-        w, h = image.size
-        max_wh = np.max([w, h])
-        hp = int((max_wh - w) / 2)
-        vp = int((max_wh - h) / 2)
-        padding = (hp, vp, hp, vp)
-        return F.pad(image, padding, 0, "constant")
+IMG_SIZE = 256
 
-def ratio_process(directory, ratio, quiet):
+def _pad_calc(H, W):
+    # There is no need to handle the parity issue of filling pixels, it will be resolved in Resize
+    p = abs(H - W) // 2
+    if H > W:
+        return (p, 0, p, 0)
+    if H < W:
+        return (0, p, 0, p)
+    else:
+        return (0, 0, 0, 0)
 
-    transform_pad = transforms.Compose([
-        transforms.ToTensor(),
-        SquarePad(),
-        transforms.Resize((256, 256)),
-        transforms.ToPILImage()
-    ])
 
-    transform_resize = transforms.Compose([
-        transforms.Resize((256, 256))
-    ])
-    
-    lens = len(list(itertools.chain.from_iterable([f for f in [files for _, _, files in os.walk(directory)]]))) - 1
+def ratio_process(directory, ratio, delete):
+    lens = len(list(itertools.chain.from_iterable([f for f in [files for _, _, files in os.walk(directory)]])))
+    tot, errs, rm_list = 0, 0, []
     with alive_bar(lens, title = Fore.WHITE + "data processing") as bar:
         for subdir, _, files in os.walk(directory):
             for file in files:
                 if file.endswith('.jpg') or file.endswith('.png'):
+                    tot += 1
                     try:
                         image_path = os.path.join(subdir, file)
                         image = Image.open(image_path)
                         width, height = image.size
                         aspect_ratio = max(width / height, height / width)
                         if aspect_ratio <= ratio:
-                            image = transform_resize(image)
+                            image = transforms.Compose([
+                                transforms.Pad(_pad_calc(height, width)),
+                                transforms.Resize((256, 256))
+                            ])(image)
                         else:
-                            image = transform_pad(image)
+                            image = transforms.Resize((256, 256))(image)
                         image.save(image_path)
                     except Exception as e:
-                        pass
-                        # if not quiet:
-                        # # print(e)
-                        #     print(Fore.LIGHTRED_EX + "Error occuring when process " + Fore.RED + str(file) + Fore.LIGHTRED_EX + ", deleting files.")
+                        errs += 1
+                        print(e)
+                        rm_list.append(image_path) if delete else None
                 bar()
+                
+    for p in rm_list:
+        os.remove(p)
+    print(Fore.LIGHTRED_EX + "{0} files have been removed".format(len(rm_list)) + Fore.WHITE) if delete else None
+    return tot, errs
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description = "")
-    parser.add_argument("--dataset_path", type = str, required = True, help = "path of your dataset")
-    parser.add_argument("--ratio", type = float, default = 1.1, help = "The aspect ratio threshold, above which the image will no longer be scaled")
-    parser.add_argument("--quiet", type = bool, default = True, help = "echo quiet")
+    parser.add_argument("-p", "--dataset_path", type = str, required = True, help = "path of your dataset")
+    parser.add_argument("-r", "--ratio", type = float, default = 1.1, help = "The aspect ratio threshold, above which the image will no longer be scaled")
+    parser.add_argument("-d", "--del", action = "store_true", help = "Delete images that failed processing")
+    # parser.add_argument("-q", "--quiet", action = "store_false", help = "echo quiet")
 
     args = parser.parse_args().__dict__
 
-    s = ratio_process(args["dataset_path"], args["ratio"], args["quiet"])
+    print(Fore.LIGHTYELLOW_EX + "Starting Process" + Fore.WHITE)
+    tot, errs = ratio_process(args["dataset_path"], args["ratio"], args["del"])
+    print(Fore.LIGHTGREEN_EX + "Successfully Completed. {_tot} in total, {_errs} errors occured".format(_errs = errs, _tot = tot) + Fore.WHITE)
